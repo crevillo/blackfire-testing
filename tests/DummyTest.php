0f8f48d6a5e457de3e7dd3c692949836d7878f86
crevillo <crevillo@gmail.com>
2017-01-27 08:52:02 +0100

    dummy test

--- tests/DummyTest.php
+++ tests/DummyTest.php
@@ -2,10 +2,32 @@
 
 namespace tests;
 
-class DummyTest extends \PHPUnit_Framework_TestCase
+use Blackfire\Bridge\PhpUnit\TestCaseTrait;
+use Blackfire\Profile;
+
+class IntegrationTest extends \PHPUnit_Framework_TestCase
 {
-    public function testDummy()
+    use TestCaseTrait;
+
+    /**
+     * @group blackfire
+     * @requires extension blackfire
+     */
+    public function testSomething()
     {
-        $this->assertEquals(1, 1);
+        $config = new Profile\Configuration();
+
+        // define some assertions
+        $config
+            ->assert('metrics.sql.queries.count < 5', 'SQL queries')
+            ->assert('metrics.twig.render.count < 3', 'Rendered Twig templates')
+            ->assert('metrics.twig.compile.count == 0', 'Twig compilation')
+            ->assert("main.wall_time < 100ms", 'muy lento')
+            // ...
+        ;
+
+        $profile = $this->assertBlackfire($config, function () {
+            file_get_contents('http://127.0.0.1:8010');
+        });
     }
 }
